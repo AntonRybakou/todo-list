1ccca147f5436579115119809eb0237a9558d9eb
ToshaBY <anton.rybakou@gmail.com>
2022-06-14 18:56:35 +0300

    docs: update README

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import AppWithReducers from "./AppWithReducers";
 import AppWithRedux from "./AppWithRedux";
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
